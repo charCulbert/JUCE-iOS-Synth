8f601c53d663043236a4279121ad683b53d7d05c
char <charlieculbertmusic@gmail.com>
2024-10-25 23:38:20 +0200

    Initial Commit

---
+++ juceStaticLibrary/juceStaticLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  juceStaticLibraryApp.swift
+//  juceStaticLibrary
+//
+//  Created by Charles Culbert on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct juceStaticLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
